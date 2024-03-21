1a3ffc5e79521d8072e82832c861ba309634e6f5
Andrel <andreltd312@gmail.com>
2024-03-21 09:26:49 -0300

    fix : added remaining packages

---
+++ src/main/java/com/lojaautopecas/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.lojaautopecas.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+
+//@WebServlet()
+public class UserController {
+}
---
+++ src/main/java/com/lojaautopecas/dao/UserDAO.java
@@ -0,0 +1,4 @@
+package com.lojaautopecas.dao;
+
+public class UserDAO {
+}
